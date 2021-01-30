bd7b57a279e1696eba40785c8699f520d7b48dfe
Guga Rukhadze <38571128+errkedukke@users.noreply.github.com>
2021-01-30 10:46:58 +0400

    Add project files.

---
+++ BubbleGumApi/Models/BubbleGumContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BubbleGumApi.Models
+{
+    public class BubbleGumContext : DbContext
+    {
+        // Options are coming from Startup.CS file
+        // services.AddDbContext<BubbleGumContext>(options => options.UseInMemoryDatabase("gum-ball"));
+
+        public BubbleGumContext(DbContextOptions<BubbleGumContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(category => category.bubbleGums)
+                .WithOne(bubblegum => bubblegum.Category)
+                .HasForeignKey(bubbleGum => bubbleGum.CategoryId);
+
+            modelBuilder.Seed();
+        }
+
+        public DbSet<BubbleGum> bubbleGums { get; set; }
+        public DbSet<Category> categories { get; set; }
+    }
+}
---
+++ BubbleGumApi/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace BubbleGumApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+        [JsonIgnore]
+        public virtual List<BubbleGum> bubbleGums { get; set; }
+    }
+}
